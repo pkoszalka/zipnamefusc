5e61f8d02afeac981cc3e9819a70672dbcb7335b
Pawel Koszalka <pawel.koszalka@yahoo.pl>
2014-09-09 22:18:45 +0200

    First working version

--- iwonka.py
+++ iwonka.py
@@ -13,13 +13,18 @@ class FilePacker(object):
 
     def __init__(self, dir_path=None):
         self.dir_path = dir_path or os.getcwd()
+        #TODO
+        self.output_dir = None
 
-    def _extract_filename(self, filename):
+    def _extract_filename_pattern(self, filename):
         """
-        ex. NMT-M-34-63-A-a-3_s.asc
+        Wyliczamy nazwe finalnego pliku
+
+        ex. NMT-M-34-63-A-a-3_s.asc => M-34-63-A-a-3
         """
         filename_str = filename.replace('NMT-', '').replace('-', '')
         name_str = filename_str[:filename_str.index('_')]
+        ext_str = filename_str.split('.')[-1]
 
         try:
             int(name_str[3:6])
@@ -30,11 +35,57 @@ class FilePacker(object):
         rejoin_array = [name_str[:1], name_str[1:3], name_str[3:3+n]]
         rejoin_array.extend(list(name_str[3+n:]))
 
-        return '-'.join(rejoin_array)
+        return '_'.join(['-'.join(rejoin_array), ext_str])
 
+    def _create_files_map(self):
+        dir_map = {}
+
+        for subdir, dirs, files in os.walk(self.dir_path):
+
+            if files:
+                dir_map[subdir] = {}
+                last_file_pattern = None
+
+            for filename in files:
+
+                if '.zip' in filename:
+                    continue
+
+                pattern = self._extract_filename_pattern(filename)
+
+                if last_file_pattern is None or last_file_pattern != pattern:
+                    last_file_pattern = pattern
+                    dir_map[subdir][pattern] = [filename, ]
+                else:
+                    dir_map[subdir][pattern].append(filename)
+
+        return dir_map
+
+    def create_zip_archive(self, archive_path, file_path, include_files):
+        print '* Creating zip archive - %s' % archive_path
+
+        with zipfile.ZipFile(archive_path, 'w') as myzip:
+            [myzip.write(os.path.join(file_path, include_file), include_file)
+             for include_file in include_files]
+
+        return myzip.filename
+
+    def create_zip_archives(self):
+        created_files = []
+
+        for dir_path, patterns in self._create_files_map().iteritems():
+            for pattern, files in patterns.iteritems():
+                archive_name = os.path.join(dir_path, '%s.zip' % pattern)
+
+                if not os.path.exists(archive_name):
+                    created_files.append(self.create_zip_archive(archive_name, dir_path, files))
+                else:
+                    print '* Archive already exists - %s' % archive_name
+
+        return created_files
 
     def run(self):
-        pass
+        return self.create_zip_archives()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,16 +1,86 @@
 #-*- coding: utf-8 -*-
 
+import os
+import shutil
+import string
 import unittest
+
 from iwonka import FilePacker
 
 
 class IwonkaTest(unittest.TestCase):
 
+    def _create_test_files_in_dir(self):
+        for dir_part in xrange(0, 3):
+            directory = os.path.join(self.tmp_dir, str(dir_part))
+            os.mkdir(directory)
+
+            for file_part in string.ascii_lowercase[:3]:
+                open(os.path.join(directory, "NMT-M-34-63-A-a-1_%s.asc" % file_part), 'w+')
+                open(os.path.join(directory, "M-24-233-A-b-1_%s.asc" % file_part), 'w+')
+                open(os.path.join(directory, 'existing_archive.zip'), 'w+')
+
+    def _build_test_dir_map(self):
+        dir_map = {}
+
+        for dir_part in xrange(0, 3):
+            directory = os.path.join(self.tmp_dir, str(dir_part))
+            dir_map[directory] = {}
+
+            files_one = ["NMT-M-34-63-A-a-1_%s.asc" % file_part for file_part in string.ascii_lowercase[:3]]
+            files_two = ["M-24-233-A-b-1_%s.asc" % file_part for file_part in string.ascii_lowercase[:3]]
+
+            dir_map[directory][self._get_filename_pattern(files_one[0])] = files_one
+            dir_map[directory][self._get_filename_pattern(files_two[0])] = files_two
+
+        return dir_map
+
+    def _get_filename_pattern(self, filename):
+        return self.packer._extract_filename_pattern(filename)
+
+    def _delete_temporary_directory(self):
+        shutil.rmtree(self.tmp_dir)
+
     def setUp(self):
-        self.packer = FilePacker()
+        self.tmp_dir = os.getcwd() + '/tmp'
+        self.packer = FilePacker(dir_path=self.tmp_dir)
+
+        if os.path.exists(self.tmp_dir):
+            self._delete_temporary_directory()
+
+        os.mkdir(self.tmp_dir)
+        self._create_test_files_in_dir()
+
+    def tearDown(self):
+        self._delete_temporary_directory()
 
     def test_filename(self):
+        """
+        Testujemy metode okreslająca nazwe pliku
+        """
+
         filename = "NMT-M-34-63-A-a-3_s.asc"
-        assert self.packer._extract_filename(filename) == 'M-34-63-A-a-3'
+        self.assertEqual(self._get_filename_pattern(filename), 'M-34-63-A-a-3_asc')
+
         filename = "M-24-233-A-b-3_s.asc"
-        assert self.packer._extract_filename(filename) == 'M-24-233-A-b-3'
+        self.assertEqual(self._get_filename_pattern(filename), 'M-24-233-A-b-3_asc')
+
+    def test_walker(self):
+        """
+        Testujemy glowna funkcjonalnosc
+
+        1. Poprawnosc tworzenia mapy przeskanowanych plikow
+        2. Poprawnosc tworzenia archiwów
+        3. W przypadku gdy archiwum istnieje - nie jest ono nadpisywane
+        """
+
+        dir_map = self.packer._create_files_map()
+        test_dir_map = self._build_test_dir_map()
+        self.assertDictEqual(dir_map, test_dir_map)
+
+        self.packer.run()
+        self.assertListEqual(self.packer.run(), [])
+
+
+if __name__ == "__main__":
+    unittest.main()
